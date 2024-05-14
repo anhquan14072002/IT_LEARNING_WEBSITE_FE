8f502cd12632ee2314ffec93012224139c933ed3
vuv909 <vuv909@gmail.com>
2024-05-15 00:16:53 +0700

    commit

---
+++ FE/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+import './App.css';
+import 'primereact/resources/primereact.min.css'; // Import PrimeReact CSS
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; // Import theme CSS
+
+export default function App() {
+  return (
+    <> 
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!!!
+      </h1>
+      <div className="card flex justify-content-center">
+        <div className="card flex flex-wrap justify-content-center gap-3">
+         
+        </div>
+      </div>
+    </>
+  );
+}
