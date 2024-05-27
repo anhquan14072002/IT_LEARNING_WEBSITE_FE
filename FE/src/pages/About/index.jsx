8e5e7100ecc2801ec118cbf3bc44ee4c384ab536
vuv909 <vuv909@gmail.com>
2024-05-27 23:38:13 +0700

    fix

---
+++ FE/src/pages/About/index.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'primereact/button'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      about
+      <Button severity='danger' label='danger' />
+    </div>
+  )
+}
