4658969e2e99bdec90e67726c658983adb073ee8
khanhddq <suttoantruot13.11.2002@gmail.com>
2024-06-25 10:11:51 +0700

    fix: update code follow tuvm

--- FE/src/utils/index.jsx
+++ FE/src/utils/index.jsx
@@ -18,7 +18,6 @@ export const formatDate = (value) => {
 };
 
 export const SUCCESS = (toast, msg = "") => {
-<<<<<<< HEAD
   toast.current.show({
     severity: "success",
     summary: "Success",
@@ -44,40 +43,12 @@ export const REJECT = (toast, msg = "") => {
     life: 3000,
   });
 };
-=======
-    toast.current.show({
-      severity: 'success',
-      summary: 'Success',
-      detail: msg || 'You have succeeded',
-      life: 3000
-    });
-  }
-  
-  export const ACCEPT = (toast, msg = "") => {
-    toast.current.show({
-      severity: 'info',
-      summary: 'Confirmed',
-      detail: msg || 'You have accepted',
-      life: 3000
-    });
-  }
-  
-  export const REJECT = (toast, msg = "") => {
-    toast.current.show({
-      severity: 'warn',
-      summary: 'Rejected',
-      detail: msg || 'You have rejected',
-      life: 3000
-    });
-  }
 
-  export const CHECKMAIL = (toast, msg = "") => {
-    toast.current.show({
-      severity: 'success',
-      summary: 'Success',
-      detail: msg || 'You should check mail to verify account',
-      life: 4000
-    });
-  }
-  
->>>>>>> 0f99a5838d5fba649ce3b9950cd514ff077cb671
+export const CHECKMAIL = (toast, msg = "") => {
+  toast.current.show({
+    severity: "success",
+    summary: "Success",
+    detail: msg || "You should check mail to verify account",
+    life: 4000,
+  });
+};
