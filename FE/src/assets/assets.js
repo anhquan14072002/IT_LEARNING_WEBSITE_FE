0c679fbcbddb592820ea872198b3023d43cbc2d8
QuanBui <quanbui030@gmail.com>
2024-08-06 00:43:14 +0700

    fixing

--- FE/src/assets/assets.js
+++ FE/src/assets/assets.js
@@ -1,4 +1,12 @@
 import image from './OIG4.jpg'
+import chart_fill from './Chart_fill.png'
+import user from './User.png'
+import folder from './Folder.png'
+import control from './control.png'
 export const assets = {
-    image
-}
+    image,
+    chart_fill,
+    user,
+    folder,
+    control
+} 
