3f5412ec55594ab66be1795d51d00128fd130b94
vuv909 <vuv909@gmail.com>
2024-08-06 16:28:03 +0700

    fix star vote

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,36 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import LoginAdmin from './pages/LoginAdmin';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import ForgotPassword from './pages/ForgotPassword';
-import Profile from './pages/Profile';
-import Search from './pages/Search';
-import CheckMail from './pages/CheckMail';
-import Document from './pages/Document';
-import Lesson from './pages/Lesson';
-import Topic from './pages/Topic';
-import ChangePassword from './pages/ChangePassword';
-import Quiz from './pages/Quiz';
-import FlashCard from './pages/FlashCard';
-import TestQuizPage from './pages/TestQuizPage';
-import ExamCodeDetail from './pages/ExamCodeDetail';
-import ExamDetail from './pages/ExamDetail';
-import ExamResult from './pages/ExamResult';
-import ManageQuestionOfQuizlist from './pages/ManageQuestionOfQuizlist';
-import AddQuestionOfQuizlist from './pages/AddQuestionOfQuizlist';
-import ViewExam from './pages/ViewExam';
-import ExampleAddQuizQuestion from './components/ExampleAddQuizQuestion';
-import CodeEditor from './pages/CodeEditor';
-import CreateProblem from './components/CreateProblem';
-import SearchQuiz from './components/SearchQuiz';
-import NotFound from './pages/NotFound';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginAdmin from "./pages/LoginAdmin";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import CheckMail from "./pages/CheckMail";
+import Document from "./pages/Document";
+import Lesson from "./pages/Lesson";
+import Topic from "./pages/Topic";
+import ChangePassword from "./pages/ChangePassword";
+import Quiz from "./pages/Quiz";
+import FlashCard from "./pages/FlashCard";
+import TestQuizPage from "./pages/TestQuizPage";
+import ExamCodeDetail from "./pages/ExamCodeDetail";
+import ExamDetail from "./pages/ExamDetail";
+import ExamResult from "./pages/ExamResult";
+import ManageQuestionOfQuizlist from "./pages/ManageQuestionOfQuizlist";
+import AddQuestionOfQuizlist from "./pages/AddQuestionOfQuizlist";
+import ViewExam from "./pages/ViewExam";
+import ExampleAddQuizQuestion from "./components/ExampleAddQuizQuestion";
+import CodeEditor from "./pages/CodeEditor";
+import CreateProblem from "./components/CreateProblem";
+import SearchQuiz from "./components/SearchQuiz";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-   
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -53,15 +51,23 @@ function App() {
         <Route path="/examcodedetail/:id" element={<ExamCodeDetail />} />
         <Route path="/examdetail/:id" element={<ExamDetail />} />
         <Route path="/examresult/:id" element={<ExamResult />} />
-        <Route path="/dashboard/quiz/managequestionofquizlist/:id" element={<ManageQuestionOfQuizlist />}/>
-        <Route path="/dashboard/quiz/addquestionofquizlist/:id" element={<AddQuestionOfQuizlist/>}/>
+        <Route
+          path="/dashboard/quiz/managequestionofquizlist/:id"
+          element={<ManageQuestionOfQuizlist />}
+        />
+        <Route
+          path="/dashboard/quiz/addquestionofquizlist/:id"
+          element={<AddQuestionOfQuizlist />}
+        />
         <Route path="/viewexam" element={<ViewExam />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/exampleAddQuizQuestion" element={<ExampleAddQuizQuestion />} />
+        <Route
+          path="/exampleAddQuizQuestion"
+          element={<ExampleAddQuizQuestion />}
+        />
         <Route path="/codeEditor/:id" element={<CodeEditor />} />
         <Route path="/dashboard/createproblem" element={<CreateProblem />} />
-
-        </Routes>
+      </Routes>
     </Router>
   );
 }
