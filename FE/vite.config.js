854b41359cd2a41e291a219a32e1d97be2497577
khanhddq <suttoantruot13.11.2002@gmail.com>
2024-08-07 22:58:38 +0700

    ffix: library

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import MillionLint from "@million/lint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    MillionLint.vite(),
+
   ],
 })
 
