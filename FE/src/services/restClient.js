5fbf7b38cdf35a09d5d4721620987fe1a6cb559d
khanhddq <suttoantruot13.11.2002@gmail.com>
2024-08-09 14:06:56 +0700

    fix : route import

--- FE/src/services/restClient.js
+++ FE/src/services/restClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "https://lw-api.azurewebsites.net";
+// export const BASE_URL = "https://lw-api.azurewebsites.net";
+export const BASE_URL = "https://localhost:7000";
 
 export default function restClient({
   url,
