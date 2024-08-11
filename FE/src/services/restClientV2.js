61f3029031c4346743dc7e2e507f9f17704a4fb6
QuanBui <quanbui030@gmail.com>
2024-08-11 23:16:06 +0700

    deploy

--- FE/src/services/restClientV2.js
+++ FE/src/services/restClientV2.js
@@ -21,3 +21,21 @@ export default function restClientV2({
     },
   });
 }
+
+// Calling the function
+const requestData = {
+  url: "languages/10",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "judge0-extra-ce.p.rapidapi.com",
+    "x-rapidapi-key": "dbc4c81344msh45e460ce8331a61p1dff48jsn75d8386e64cf"
+  }
+};
+
+restClientV2(requestData)
+  .then(response => {
+    console.log("Response:", response.data);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
