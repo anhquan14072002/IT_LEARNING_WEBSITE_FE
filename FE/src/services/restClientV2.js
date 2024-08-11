264112c5d4c819facf4a1c41c65f5707fb89ad47
QuanBui <quanbui030@gmail.com>
2024-08-11 23:11:33 +0700

    build

--- FE/src/services/restClientV2.js
+++ FE/src/services/restClientV2.js
@@ -7,22 +7,17 @@ export default function restClientV2({
   method = "GET",
   params,
   data,
-  headers,
+  headers = {},
 }) {
   return axios({
     url: `${BASE_URL}/${url}`,
     method,
     params,
     data,
-    headers: [
-      {
-        key: "x-rapidapi-host",
-        value: "judge0-extra-ce.p.rapidapi.com",
-      },
-      {
-        key: "x-rapidapi-key",
-        value: "dbc4c81344msh45e460ce8331a61p1dff48jsn75d8386e64cf",
-      },
-    ],
+    headers: {
+      ...headers,
+      "x-rapidapi-host": "judge0-extra-ce.p.rapidapi.com",
+      "x-rapidapi-key": "dbc4c81344msh45e460ce8331a61p1dff48jsn75d8386e64cf",
+    },
   });
 }
