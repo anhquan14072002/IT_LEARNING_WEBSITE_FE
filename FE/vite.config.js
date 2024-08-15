90e254b69011d5d57f31c7d64612cec9efa79e72
QuanBui <quanbui030@gmail.com>
2024-08-15 16:31:52 +0700

    fix

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), // No need to specify jsxRuntime for React 18
-  ],
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  mode,
+  build: {
+    minify: mode === 'production' ? 'esbuild' : false, // Minify only in production
+  },
+}));
