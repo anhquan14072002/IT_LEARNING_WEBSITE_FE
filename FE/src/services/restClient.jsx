b7a89839c37893e8f3681ccfc702a83d46f0f35e
QuanBui <quanbui030@gmail.com>
2024-08-25 22:40:27 +0700

    fix

--- FE/src/services/restClient.jsx
+++ FE/src/services/restClient.jsx
@@ -1,21 +1,25 @@
 import axios from "axios";
-
+import { getTokenFromLocalStorage } from "../utils";
 
 export const BASE_URL = "http://localhost:8000";
-
 
 export default function restClient({
   url,
   method = "GET",
-  params,
-  data,
-  headers
+  params = {},
+  data = {},
+  headers = {},
 }) {
+  const token = getTokenFromLocalStorage();
+
   return axios({
-    url: `${BASE_URL}/${url}`,
+    url: `${BASE_URL}/${url}`, // Fixed the template string syntax
     method,
     params,
     data,
-    headers 
-  })
+    headers: {
+      ...headers,
+      ...(token && { Authorization: `Bearer ${token}` }), // Only add Authorization header if the token exists
+    },
+  });
 }
