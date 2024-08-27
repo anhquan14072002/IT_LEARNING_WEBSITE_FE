f7b584ddafc444594566e866a9653fe6533a3936
vuv909 <vuv909@gmail.com>
2024-08-27 09:15:06 +0700

    update

--- FE/src/components/AddSolution/index.jsx
+++ FE/src/components/AddSolution/index.jsx
@@ -10,12 +10,14 @@ import "codemirror/theme/material.css";
 import { Editor } from "primereact/editor";
 import { useSelector } from "react-redux";
 import restClient from "../../services/restClient";
+import Loading from "../Loading";
 
 const AddSoltution = ({ visible, setVisible, toast, id, fetchSolutions }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [code, setCode] = useState("");
   const user = useSelector((state) => state.user.value);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {
     if (title.trim().length < 5) {
@@ -26,13 +28,14 @@ const AddSoltution = ({ visible, setVisible, toast, id, fetchSolutions }) => {
       REJECT(toast, "Vui lòng nhập tiêu đề nhỏ hơn 250 kí tự");
       return;
     }
-    if (!title.trim() || !description.trim() || !code.trim()) {
+    if (!title.trim() || !description || ( typeof(description) === "string" && !description.trim() ) || !code.trim() ) {
       REJECT(
         toast,
         "Vui lòng không để trống các trường đánh dấu bắt buộc nhập"
       );
       return;
     }
+    setLoading(true);
     restClient({
       url: "api/solution/createsolution",
       method: "POST",
@@ -57,81 +60,90 @@ const AddSoltution = ({ visible, setVisible, toast, id, fetchSolutions }) => {
         setTitle("");
         setDescription("");
         setCode("");
+        setLoading(false);
       });
   };
 
   return (
     <Dialog
-      header="Thêm lời giải"
+      header="Thêm bình luận"
       visible={visible}
       style={{ width: "50vw" }}
       onHide={() => {
         setVisible(false);
       }}
     >
-      <div className="mb-5">
-        <h1>
-          Tiêu đề<span className="text-red-500">*</span>
-        </h1>
-        <input
-          type="text"
-          className="w-full p-2 border border-gray-400 rounded-lg"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="mb-5">
+            <h1>
+              Tiêu đề<span className="text-red-500">*</span>
+            </h1>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-400 rounded-lg"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
 
-      <div>
-        <h1>Mô tả</h1>
-        <Editor
-          style={{ height: "300px" }}
-          className="mb-5 border"
-          value={description}
-          onTextChange={(e) => setDescription(e.htmlValue)}
-        />
-      </div>
+          <div>
+            <h1>
+              Mô tả<span className="text-red-500">*</span>
+            </h1>
+            <Editor
+              style={{ height: "300px" }}
+              className="mb-5 border"
+              value={description}
+              onTextChange={(e) => setDescription(e.htmlValue)}
+            />
+          </div>
 
-      <div className="mb-5">
-        <h1>
-          Lời giải <span className="text-red-500">*</span>
-        </h1>
-        <CodeMirror
-          value={code}
-          options={{
-            theme: "material",
-            lineNumbers: true,
-          }}
-          onBeforeChange={(editor, data, value) => {
-            setCode(value);
-          }}
-          editorDidMount={(editor) => {
-            editor.setSize(null, "calc(50vh - 5px)");
-          }}
-        />
-      </div>
+          <div className="mb-5">
+            <h1>
+              Lời giải <span className="text-red-500">*</span>
+            </h1>
+            <CodeMirror
+              value={code}
+              options={{
+                theme: "material",
+                lineNumbers: true,
+              }}
+              onBeforeChange={(editor, data, value) => {
+                setCode(value);
+              }}
+              editorDidMount={(editor) => {
+                editor.setSize(null, "calc(50vh - 5px)");
+              }}
+            />
+          </div>
 
-      <div className="flex justify-end gap-5">
-        <Button
-          className="p-2 bg-red-500 text-white"
-          type="button"
-          severity="danger"
-          onClick={() => {
-            setVisible(false);
-            setTitle("");
-            setDescription("");
-            setCode("");
-          }}
-        >
-          Hủy
-        </Button>
-        <Button
-          className="p-2 bg-blue-500 text-white"
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Thêm
-        </Button>
-      </div>
+          <div className="flex justify-end gap-5">
+            <Button
+              className="p-2 bg-red-500 text-white"
+              type="button"
+              severity="danger"
+              onClick={() => {
+                setVisible(false);
+                setTitle("");
+                setDescription("");
+                setCode("");
+              }}
+            >
+              Hủy
+            </Button>
+            <Button
+              className="p-2 bg-blue-500 text-white"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Thêm
+            </Button>
+          </div>
+        </>
+      )}
     </Dialog>
   );
 };
